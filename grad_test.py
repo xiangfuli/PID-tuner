Review vee and hat to get correct gradient computation

grad_test.py
@@ -12,28 +12,21 @@ grad = jacobian(quaternion_2_rotation_matrix, a)
 # print(grad)
 
 def func(input0, input1):
-  a = input0[0]
-  b = input0[1]
+  a = input0 ** 2
 
   c = input1[0]
   d = input1[1]
 
-  a_pow = a ** 2
-  b_pow = b ** 2
   c_pow = c ** 2
   d_pow = d ** 2
 
   # return torch.tensor(
-  #   [
-  #     a_pow,
-  #     b_pow,
-  #     c_pow,
-  #     d_pow,
-  #   ]
+  #     a
   # )
-  return (a_pow, b_pow, c_pow, d_pow)
+  return torch.stack((a, a))
+  # return (a, c_pow, d_pow)
 
-grad = jacobian(func, (torch.tensor([2,3]).double(), torch.tensor([5,6]).double()))
+grad = jacobian(func, (torch.tensor([2, 3]).double(), torch.tensor([5,6]).double()))
 
 print(grad)
 

utils/commons.py
@@ -7,24 +7,23 @@ def get_tensor_item(tensor):
     return 0.
 
 def hat(vector):
-  return torch.tensor([
-    [0, -vector[2], vector[1]],
-    [vector[2], 0, -vector[0]],
-    [-vector[1], vector[0], 0]
-  ])
+  vector = vector.reshape([3, 1])
+  return torch.stack([
+    torch.stack([torch.tensor([0.]), -vector[2], vector[1]], dim=0),
+    torch.stack([vector[2], torch.tensor([0.]), -vector[0]], dim=0),
+    torch.stack([-vector[1], vector[0], torch.tensor([0.])], dim=0)
+  ]).reshape([3, 3])
 
 def vee(skew_symmetric_matrix):
-  return torch.tensor(
-    [
-      -skew_symmetric_matrix[1][2],
-      skew_symmetric_matrix[0][2],
-      -skew_symmetric_matrix[0][1],
-    ]
+  return torch.stack(
+      (-skew_symmetric_matrix[1, 2],
+      skew_symmetric_matrix[0, 2],
+      -skew_symmetric_matrix[0, 1])
   ).reshape([3, 1])
 
 def quaternion_2_rotation_matrix(q):
   q = q / torch.norm(q)
-  qahat = hat(q[:][1:4])
+  qahat = hat(q[1:4])
   return (torch.eye(3) + 2 * torch.mm(qahat, qahat) + 2 * q[0] * qahat).double()
 
 def rotation_matrix_2_quaternion(R):
@@ -74,16 +73,12 @@ def get_desired_angular_speed(original_ori, des_ori, dt):
   return get_shortest_path_between_angles(original_ori, des_ori) / dt
 
 def angular_vel_2_quaternion_dot(quaternion, w):
-  qW, qX, qY, qZ = quaternion
   p, q, r = w
-
-  K_quat = 2; #this enforces the magnitude 1 constraint for the quaternion
-  quaterror = 1 - (qW**2 + qX**2 + qY**2 + qZ**2)
-  
-  return -0.5 * torch.mm(torch.tensor(
+  zero_t = torch.tensor([0.])
+  return -0.5 * torch.mm(torch.stack(
     [
-      [0, p, q, r],
-      [-p, 0, -r, q],
-      [-q, r, 0, -p],
-      [-r, -q, p, 0]
-    ], dtype=torch.float64), quaternion)
+      torch.stack([zero_t, p, q, r]),
+      torch.stack([-p, zero_t, -r, q]),
+      torch.stack([-q, r, zero_t, -p]),
+      torch.stack([-r, -q, p, zero_t])
+    ]).reshape([4, 4]), quaternion)